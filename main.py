ffcaed2ed919f12a3702adc4dd01a53cf8c3f88b
FlamestarRS <FlamestarRS@gmail.com>
2025-01-25 18:06:47 -0500

    created gameloop

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 import pygame
+from constants import *
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    print("Pizza Time")
+
+    while True:
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0,0,0))
+
+
+
+
+if __name__ == "__main__":
+    main()
