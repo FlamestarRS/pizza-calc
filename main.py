9ed91514dd544d4baca31ca8d0c51d9d5412204c
FlamestarRS <FlamestarRS@gmail.com>
2025-01-26 04:08:35 -0500

    added area/diameter math, user input, and result

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from pizza import *
 
 def main():
     pygame.init()
@@ -7,6 +8,23 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     print("Pizza Time")
+
+    first_pizza = Pizza(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    second_pizza = Pizza(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    third_pizza = Pizza(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    first_area = first_pizza.area(float(input("How big is the first pizza?")))
+    print(first_area)
+
+    second_area = second_pizza.area(float(input("How big is the second pizza?")))
+    print(second_area)
+
+    difference = round(first_area - second_area, 2)
+    third_area = round(third_pizza.diameter(difference), 2)
+
+    print(f"The first pizza is {difference} square inches bigger than the second pizza")
+    print(f"This is equivalent to another {third_area} inch pizza")
+
 
     while True:
         
--- pizza.py
+++ pizza.py
@@ -1,11 +1,19 @@
 import pygame
+import math
 from circleshape import *
 
 
 class Pizza(CircleShape):
-    def __init__(self, x, y, radius):
-        super().__init__(x, y, radius)
+    def __init__(self, x, y):
+        super().__init__(x, y, 5)
+        #self.radius = radius
 
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+
+    def area(self, d):
+        return math.pi * d
+    
+    def diameter(self, area):
+        return area / math.pi
